174f13111b1bc3b6a9c854e1f51dd79a380dd508
mattie <m_alrod@cda118ev5635x.local>
2019-01-23 12:14:34 -0500

    added unity practice folder

---
+++ unity practice/rollballgame/Assets/_Completed-Game/Scripts/MattieCameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MattieCameraControl : MonoBehaviour {
+// we want the camera to be a little ways away from the player and not rotate like im being thrown into a washing machine
+
+
+	public GameObject player;
+
+	//offset private so it cant be touched in the editor i think???
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		//late update makes it so that the camera updates after the player has moved, for sure
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ unity practice/rollballgame/Assets/_Completed-Game/Scripts/MattiePlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MattiePlayerController : MonoBehaviour {
+
+	//what do we want? for program to check for input every frame and change player position based on that
+
+	//variable for changing movement speed
+	public float speed;
+
+	//what the hell is this is this how u set variables in C# help
+	private Rigidbody rb;
+
+	// i can only assume that this is like a setup function
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		// Input is a class, has many variables to support it
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		//Vector3 variable
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+
+		// moving the ball relies on applying force to the ball's rigidbody component
+		rb.AddForce(movement * speed);
+
+	}
+}
+
+// NOTES & QUESTIONS:
+// - whats the difference between update and fixedupdate ?
+// - floats are just variables
+// - goddamn i need to read up on C# syntax i have no idea what anything is
+// - YOOOOOOOO IF YOU MAKE A PUBLIC VARIABLE IT JUST STRAIGHT UP SHOWS UP INT HE UNITY INSPECTOR IM LOSING MY SHIT
